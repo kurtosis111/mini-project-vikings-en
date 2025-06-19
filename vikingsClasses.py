99d8571f390626ae80e0d014e9b3314116b26794
Xingyu Yang <x.yang94@outlook.com>
2025-06-19 22:25:26 +0200

    mini project 1

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -6,12 +6,17 @@ import random
 class Soldier:
     def __init__(self, health, strength):
         # your code here
+        self.health = health
+        self.strength = strength
     
     def attack(self):
         # your code here
+        return self.strength
 
     def receiveDamage(self, damage):
         # your code here
+        self.health -= damage
+        
     
 
 # Viking
@@ -19,42 +24,80 @@ class Soldier:
 class Viking(Soldier):
     def __init__(self, name, health, strength):
         # your code here
+        self.name = name
+        super().__init__(health, strength)
 
     def battleCry(self):
         # your code here
+        return "Odin Owns You All!"
+        
 
     def receiveDamage(self, damage):
         # your code here
+        self.health -= damage
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return f"{self.name} has died in act of combat"
 
 # Saxon
 
 class Saxon(Soldier):
     def __init__(self, health, strength):
         # your code here
+        super().__init__(health, strength)
 
     def receiveDamage(self, damage):
         # your code here
+        self.health -= damage
+        if self.health > 0:
+            return f"A Saxon has received {damage} points of damage"
+        else:
+            return f"A Saxon has died in combat"
 
 # Davicente
 
 class War():
     def __init__(self):
         # your code here
+        self.vikingArmy = []
+        self.saxonArmy = []
 
     def addViking(self, viking):
         # your code here
+        self.vikingArmy.append(viking)
     
     def addSaxon(self, saxon):
         # your code here
+        self.saxonArmy.append(saxon)
     
     def vikingAttack(self):
         # your code here
+        chosen_viking_idx = random.sample(range(len(self.vikingArmy)), 1)[0]
+        chosen_saxon_idx = random.sample(range(len(self.saxonArmy)), 1)[0]
+        received_damage = self.saxonArmy[chosen_saxon_idx].receiveDamage(self.vikingArmy[chosen_viking_idx].strength)
+        if received_damage == f"A Saxon has died in combat":
+            del self.saxonArmy[chosen_saxon_idx]
+        return received_damage
     
     def saxonAttack(self):
         # your code here
+        chosen_viking_idx = random.sample(range(len(self.vikingArmy)), 1)[0]
+        chosen_viking = self.vikingArmy[chosen_viking_idx]
+        chosen_saxon_idx = random.sample(range(len(self.saxonArmy)), 1)[0]
+        received_damage = chosen_viking.receiveDamage(self.saxonArmy[chosen_saxon_idx].strength)
+        if received_damage == f"{chosen_viking.name} has died in act of combat":
+            del self.vikingArmy[chosen_viking_idx]
+        return received_damage
 
     def showStatus(self):
         # your code here
+        if len(self.saxonArmy) == 0:
+            return f"Vikings have won the war of the century!"
+        elif len(self.vikingArmy) == 0:
+            return f"Saxons have fought for their lives and survive another day..."
+        elif (len(self.saxonArmy)==1) and (len(self.vikingArmy)==1):
+            return f"Vikings and Saxons are still in the thick of battle."
     pass
 
 
